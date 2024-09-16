1615a455abe924713efbe04a00460efaab8e8a52
unknown <abhishekmishra992016@gmail.com>
2024-09-16 09:44:08 +0530

    refine all the components

--- src/layouts/auth/index.jsx
+++ src/layouts/auth/index.jsx
@@ -1,7 +1,6 @@
 import Footer from "components/footer/FooterAuthDefault";
 import { Routes, Route, Navigate } from "react-router-dom";
 import routes from "routes.js";
-import FixedPlugin from "components/fixedPlugin/FixedPlugin";
 
 export default function Auth() {
 
@@ -21,7 +20,6 @@ export default function Auth() {
   return (
     <div>
       <div className="relative float-right h-full min-h-screen w-full !bg-white dark:!bg-navy-900">
-        <FixedPlugin />
         <main className={`mx-auto min-h-screen`}>
           <div className="relative flex">
             <div className="mx-auto flex min-h-full w-full flex-col justify-start pt-12 md:max-w-[75%]  lg:max-w-[1013px] lg:px-8 lg:pt-0 xl:min-h-[100vh] xl:max-w-[1383px] xl:px-0 xl:pl-[70px]">
