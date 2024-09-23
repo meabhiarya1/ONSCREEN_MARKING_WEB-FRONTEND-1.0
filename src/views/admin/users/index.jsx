9c6e0ad9dd3d6e6312ea58bacc5355819999c34f
unknown <abhishekmishra992016@gmail.com>
2024-09-23 16:44:51 +0530

    ui changes

--- src/views/admin/users/index.jsx
+++ src/views/admin/users/index.jsx
@@ -21,7 +21,7 @@ const Index = () => {
       }
     };
     fetchUsers();
-  }, [isOpen, navigate]); 
+  }, [isOpen, navigate]);
 
   // Specify the keys you want to display
   const visibleFields = ["name", "email", "mobile", "role", "date"];
@@ -54,68 +54,68 @@ const Index = () => {
 
   return (
     <div className="mt-12 overflow-x-auto">
-    <table className="min-w-full table-auto divide-y divide-gray-300 bg-white text-sm">
-      <thead className="bg-gray-100">
-        <tr>
-          {visibleFields.map((key) => (
-            <th
-              key={key}
-              className="whitespace-nowrap px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              {key.charAt(0).toUpperCase() + key.slice(1)}
+      <table className="min-w-full table-auto divide-y divide-gray-300 bg-white text-sm">
+        <thead className="bg-gray-100">
+          <tr>
+            {visibleFields.map((key) => (
+              <th
+                key={key}
+                className="whitespace-nowrap px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                {key.charAt(0).toUpperCase() + key.slice(1)}
+              </th>
+            ))}
+            <th className="whitespace-nowrap px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Edit
             </th>
-          ))}
-          <th className="whitespace-nowrap px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Edit
-          </th>
-          <th className="whitespace-nowrap px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Remove
-          </th>
-        </tr>
-      </thead>
-      
-      <tbody className="divide-y divide-gray-200 bg-white">
-        {users &&
-          users.map((user) => (
-            <tr key={user._id} className="bg-white hover:bg-gray-50">
-              {visibleFields.map((field) => (
-                <td key={field} className="whitespace-nowrap px-6 py-4 text-sm text-gray-700">
-                  {field === "date"
-                    ? new Date(user[field]).toLocaleDateString()
-                    : user[field]}
-                </td>
-              ))}
-  
-              <td className="whitespace-nowrap px-6 py-4">
-                <button
-                  className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-                  onClick={() => handleClick(user)}
-                >
-                  Edit
-                </button>
-              </td>
-  
-              {user._id !== localStorage.getItem("userId") && (
+            <th className="whitespace-nowrap px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Remove
+            </th>
+          </tr>
+        </thead>
+
+        <tbody className="divide-y divide-gray-200 bg-white">
+          {users &&
+            users.map((user) => (
+              <tr key={user._id} className="bg-white hover:bg-gray-50">
+                {visibleFields.map((field) => (
+                  <td key={field} className="whitespace-nowrap px-6 py-4 text-sm text-gray-700">
+                    {field === "date"
+                      ? new Date(user[field]).toLocaleDateString()
+                      : user[field]}
+                  </td>
+                ))}
+
                 <td className="whitespace-nowrap px-6 py-4">
                   <button
-                    className="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white hover:bg-red-700"
-                    onClick={() => onClickRemove(user._id)}
+                    className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+                    onClick={() => handleClick(user)}
                   >
-                    Remove
+                    Edit
                   </button>
                 </td>
-              )}
-            </tr>
-          ))}
-      </tbody>
-    </table>
-  
-    {/* Render the modal when isOpen is true */}
-    {isOpen && (
-      <Modal user={selectedUser} isOpen={isOpen} setIsOpen={setIsOpen} />
-    )}
-  </div>
-  
+
+                {user._id !== localStorage.getItem("userId") && (
+                  <td className="whitespace-nowrap px-6 py-4">
+                    <button
+                      className="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white hover:bg-red-700"
+                      onClick={() => onClickRemove(user._id)}
+                    >
+                      Remove
+                    </button>
+                  </td>
+                )}
+              </tr>
+            ))}
+        </tbody>
+      </table>
+
+      {/* Render the modal when isOpen is true */}
+      {isOpen && (
+        <Modal user={selectedUser} isOpen={isOpen} setIsOpen={setIsOpen} />
+      )}
+    </div>
+
   );
 };
 
