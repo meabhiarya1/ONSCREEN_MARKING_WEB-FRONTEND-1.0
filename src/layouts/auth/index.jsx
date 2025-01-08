001c85029cb44492589aeb1a172d9cc38dbba882
Sparsh-Jain-531 <sparshjain531@gmail.com>
2025-01-08 16:43:04 +0530

    feat: [Add NotFound components and implement routing for 404 pages in Admin, Auth, and Evaluator layouts] ,style: [enhance AssignModal with loading state].

--- src/layouts/auth/index.jsx
+++ src/layouts/auth/index.jsx
@@ -1,8 +1,8 @@
+import NotFoundOuter from "layouts/NotFound/Admin/NotFoundOuter";
 import { Routes, Route } from "react-router-dom";
 import SignIn from "views/auth/SignIn";
 
 export default function Auth() {
-
   return (
     <div className="relative float-right h-full min-h-screen w-full bg-white dark:bg-navy-900 ">
       <main className="mx-auto min-h-screen">
@@ -10,6 +10,7 @@ export default function Auth() {
           <div className="flex w-full flex-col items-center justify-center bg-indigo-50">
             <Routes>
               <Route path={`/sign-in`} element={<SignIn />} />
+              <Route path="*" element={<NotFoundOuter />} />
             </Routes>
           </div>
         </div>
