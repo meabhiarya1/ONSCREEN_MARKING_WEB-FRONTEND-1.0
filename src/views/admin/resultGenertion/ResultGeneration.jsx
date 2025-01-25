bfbba7e26c93eed3b6d0deb69976bc29465914de
Sparsh-Jain-531 <sparshjain531@gmail.com>
2025-01-25 11:49:54 +0530

    feat: enhance UI with new button styles and update button text for clarity

--- src/views/admin/resultGenertion/ResultGeneration.jsx
+++ src/views/admin/resultGenertion/ResultGeneration.jsx
@@ -301,7 +301,7 @@ const ResultGeneration = () => {
     try {
       const response = await sendData(file);
       if (response?.data?.message) {
-        toast.success(response.data?.message);
+        toast.error(response.data?.message);
         downloadFile(response?.data);
       } else {
         toast.warning("Unexpected response format");
@@ -429,14 +429,14 @@ const ResultGeneration = () => {
 
                 <div className="mt-2 flex items-center justify-between gap-1">
                   <div
-                    className="sm:text-md group mt-4 inline-flex items-center gap-2 rounded-lg bg-indigo-500 px-2 py-1.5 text-sm font-medium text-white transition-all hover:bg-indigo-600 lg:text-lg"
+                    className="sm:text-md group mt-4 inline-flex items-center rounded-lg bg-indigo-500 px-2 py-1.5 text-sm font-medium text-white transition-all hover:bg-indigo-600 lg:text-lg"
                     onClick={downloadSampleCsv}
                   >
                     <span>Download Sample</span>
                     <FaArrowAltCircleDown className="m-1 text-lg" />
                   </div>
 
-                  <div className="mt-4 inline-flex items-center gap-2 rounded-lg bg-indigo-500 px-2 py-1 text-white hover:bg-indigo-600">
+                  <div className="mt-4 inline-flex items-center rounded-lg bg-indigo-500 px-2 py-1 text-white hover:bg-indigo-600">
                     <label
                       className="sm:text-md group inline-flex cursor-pointer items-center gap-1 text-sm font-medium text-white transition-all lg:text-lg"
                       htmlFor="uploadCSV"
@@ -673,11 +673,27 @@ const ResultGeneration = () => {
         {newResult?.length > 0 && (
           <div className="mt-12 flex flex-col">
             {" "}
-            <div className="flex justify-end">
+            <div className="flex justify-end gap-5">
+            <button
+                className="button flex justify-center items-center"
+                type="submit"
+              >
+                <svg
+                  viewBox="0 0 576 512"
+                  class="svg"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M234.7 42.7L197 56.8c-3 1.1-5 4-5 7.2s2 6.1 5 7.2l37.7 14.1L248.8 123c1.1 3 4 5 7.2 5s6.1-2 7.2-5l14.1-37.7L315 71.2c3-1.1 5-4 5-7.2s-2-6.1-5-7.2L277.3 42.7 263.2 5c-1.1-3-4-5-7.2-5s-6.1 2-7.2 5L234.7 42.7zM46.1 395.4c-18.7 18.7-18.7 49.1 0 67.9l34.6 34.6c18.7 18.7 49.1 18.7 67.9 0L529.9 116.5c18.7-18.7 18.7-49.1 0-67.9L495.3 14.1c-18.7-18.7-49.1-18.7-67.9 0L46.1 395.4zM484.6 82.6l-105 105-23.3-23.3 105-105 23.3 23.3zM7.5 117.2C3 118.9 0 123.2 0 128s3 9.1 7.5 10.8L64 160l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L128 160l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L128 96 106.8 39.5C105.1 35 100.8 32 96 32s-9.1 3-10.8 7.5L64 96 7.5 117.2zm352 256c-4.5 1.7-7.5 6-7.5 10.8s3 9.1 7.5 10.8L416 416l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L480 416l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L480 352l-21.2-56.5c-1.7-4.5-6-7.5-10.8-7.5s-9.1 3-10.8 7.5L416 352l-56.5 21.2z"
+                    fill="#74C0FC"
+                  ></path>
+                </svg>
+                Generate by AI
+              </button>
               <button
                 class="button"
                 type="submit"
-                className="mr-5 flex w-40 items-center justify-center gap-2 rounded-md bg-indigo-500 p-2 text-lg text-white hover:bg-indigo-600"
+                className="mr-5 flex w-40 items-center justify-center gap-2 rounded-md bg-indigo-500 p-2 text-lg text-white hover:bg-indigo-600 button1"
                 onClick={() => {
                   setNewResult([]);
                   setPreviousResults([]);
@@ -693,7 +709,7 @@ const ResultGeneration = () => {
                     fill="#74C0FC"
                   ></path>
                 </svg>
-                Generate New
+                Generate Result
               </button>
             </div>
             <div
